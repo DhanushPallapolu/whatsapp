7e978c9df476532b5c07020284c505f368f2573d
Cute Coding <83330007+DhanushPallapolu@users.noreply.github.com>
2021-07-16 16:12:15 +0530

    Update Loading.js

--- components/Loading.js
+++ components/Loading.js
@@ -1,10 +1,11 @@
 import { Circle } from "better-react-spinkit";
+import Img from "next/image"
 
 function Loading() {
   return (
     <center style={{ display: "grid", placeItems: "center", height: "100vh" }}>
       <div>
-        <img
+        <Img
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/1200px-WhatsApp.svg.png"
           alt="Loading"
           style={{ marginBottom: 10 }}
