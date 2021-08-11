81b01564f9e47f24288243c8edc494c04e7b4e9f
Cute Coding <83330007+DhanushPallapolu@users.noreply.github.com>
2021-08-11 14:40:23 +0530

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -37,7 +37,7 @@ const LoginContainer = styled.div`
   align-items: center;
   background-color: white;
   border-radius: 5px;
-  box-shadow: 0px 4px 14px -3px rgba(0, 0, 0, 0.7) #000;
+
 `;
 const Logo = styled.img`
   height: 200px;
