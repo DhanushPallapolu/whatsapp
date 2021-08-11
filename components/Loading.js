48874413d3ef75fb7a730fee729e6e071c166493
Cute Coding <83330007+DhanushPallapolu@users.noreply.github.com>
2021-08-11 14:28:26 +0530

    Update Loading.js

--- components/Loading.js
+++ components/Loading.js
@@ -6,7 +6,7 @@ function Loading() {
     <center style={{ display: "grid", placeItems: "center", height: "100vh" }}>
       <div>
         <Img
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/1200px-WhatsApp.svg.png"
+          src="https://firebasestorage.googleapis.com/v0/b/whatsapp-sp.appspot.com/o/spchat.png?alt=media&token=be0717fb-54a5-4e4d-836f-3aefae34a5cb"
           alt="Loading"
           style={{ marginBottom: 10 }}
           height={200}
