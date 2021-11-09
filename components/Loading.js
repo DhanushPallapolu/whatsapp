cd19f9cf032ab91a571a2513366f8dafdfd14c49
Cute Coding <83330007+DhanushPallapolu@users.noreply.github.com>
2021-11-09 18:46:12 +0530

    Update Loading.js

--- components/Loading.js
+++ components/Loading.js
@@ -2,11 +2,12 @@ import { Circle } from "better-react-spinkit";
 import styled from "styled-components"
 
 function Loading() {
+  const img = require('./spchat.png')
   return (
     <center style={{ display: "grid", placeItems: "center", height: "100vh" }}>
       <div>
         <Img
-          src="https://firebasestorage.googleapis.com/v0/b/whatsapp-sp.appspot.com/o/spchat.png?alt=media&token=be0717fb-54a5-4e4d-836f-3aefae34a5cb"
+          src={img}
           alt="Loading"
           style={{ marginBottom: 10 }}
           height={200}
