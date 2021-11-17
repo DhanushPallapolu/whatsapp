768aafb2c9b39f4659ea7e1f8621e37dcdb129ea
Cute Coding <83330007+DhanushPallapolu@users.noreply.github.com>
2021-11-17 12:18:53 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
     <div>
       <Head>
         <title>Spronklle Chat</title>
-        <meta name="description" content="A all new 1:1 personal chatting, only with you email address!" />
+        <meta name="description" content="A all new 1:1 personal chatting, only with you email address! No phone number required!" />
         <link rel="icon" href="https://firebasestorage.googleapis.com/v0/b/whatsapp-sp.appspot.com/o/spchat.png?alt=media&token=be0717fb-54a5-4e4d-836f-3aefae34a5cb" />
       </Head>
       <Sidebar />
